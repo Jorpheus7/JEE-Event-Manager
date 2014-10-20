ed2f3175ac87835bceddeb664bbfa2cdba7d1b2b
Thibault <thibault.devoye@hotmail.fr>
2014-10-20 14:34:21 +0200

    Ajout de la création d'évenement

---
+++ src/fr/jee/event/NewEvent.java
@@ -0,0 +1,42 @@
+package fr.jee.event;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Event
+ */
+public class NewEvent extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewEvent() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	private void process(HttpServletRequest request,HttpServletResponse response)  throws ServletException, IOException{
+		getServletContext().getRequestDispatcher("/jsp/event.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+}
